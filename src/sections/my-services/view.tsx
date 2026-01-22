4c4f56e9184112587f968d38f3feb44219d7e2ea
ahmed <mido1esam2@gmail.com>
2026-01-22 08:49:07 +0200

    fix

--- src/sections/my-services/view.tsx
+++ src/sections/my-services/view.tsx
@@ -10,7 +10,7 @@ export default function MyServicesView() {
     <>
       <AboutUsHero />
       <OrdersSection />
-      <SupportSection />
+      {/* <SupportSection /> */}
       <LocationSection />
       <SupportSection />
     </>
